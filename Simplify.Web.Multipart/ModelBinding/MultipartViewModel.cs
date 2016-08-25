99d419e6f5bb837a907bc8cfe9f2abb0ca529c69
Alexanderius <alexander.krylkov@gmail.com>
2016-08-25 16:08:48 +0600

    XML comments fixed

--- Simplify.Web.Multipart/ModelBinding/MultipartViewModel.cs
+++ Simplify.Web.Multipart/ModelBinding/MultipartViewModel.cs
@@ -3,10 +3,25 @@ using HttpMultipartParser;
 
 namespace Simplify.Web.Multipart.ModelBinding
 {
+	/// <summary>
+	/// HTTP multipart form data model
+	/// </summary>
 	public class MultipartViewModel
 	{
+		/// <summary>
+		/// HTTP multipart form data files
+		/// </summary>
+		/// <value>
+		/// The files.
+		/// </value>
 		public List<FilePart> Files { get; set; }
 
+		/// <summary>
+		/// HTTP multipart form data parameters
+		/// </summary>
+		/// <value>
+		/// The parameters.
+		/// </value>
 		public List<ParameterPart> Parameters { get; set; }
 	}
 }
