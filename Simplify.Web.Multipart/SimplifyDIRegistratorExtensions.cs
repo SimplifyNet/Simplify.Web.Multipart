eb29e284c2a38a52b6104c430839f5efc13ad538
Alexanderius <alexander.krylkov@gmail.com>
2019-10-21 23:17:40 +0600

    Fixes #3

---
+++ Simplify.Web.Multipart/SimplifyDIRegistratorExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Simplify.DI;
+using Simplify.Web.Multipart.Model.Binding;
+
+namespace Simplify.Web.Multipart
+{
+	/// <summary>
+	/// Provides Simplify.Web.Json default registration
+	/// </summary>
+	public static class SimplifyDIRegistratorExtensions
+	{
+		/// <summary>
+		/// Registers Simplify.Web.Json JsonModelBinder.
+		/// </summary>
+		/// <param name="registrator">The registrator.</param>
+		public static IDIRegistrator RegisterHttpMultipartFormModelBinder(this IDIRegistrator registrator)
+		{
+			registrator.Register<HttpMultipartFormModelBinder>(LifetimeType.Singleton);
+
+			return registrator;
+		}
+	}
+}
